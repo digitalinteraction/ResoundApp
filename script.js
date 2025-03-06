5e323d8793d427e266a035983e6e1c6a41b61a0f
David Chatting <david@davidchatting.com>
2025-03-06 11:44:39 +0000

    Update script.js

--- script.js
+++ script.js
@@ -303,7 +303,7 @@ async function onSlideMoved() {
 async function activateTuning(v = true) {
     const button = document.getElementById('tune_button');
 
-    if (v) {
+    if (v && webSocketConnected) {
         tuning = true; // Set tuning to true - samples will be added to histogram
         button.classList.add('active'); // Add the active class
         filter = {
