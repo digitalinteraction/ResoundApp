9e3bea1a741f296277142f2a88017d4173208dbe
David Chatting <david@davidchatting.com>
2025-12-08 18:25:20 +0000

    Update script.js

--- script.js
+++ script.js
@@ -364,7 +364,7 @@ function onOnline() {
     console.log("onOnline");
     rebootTimeoutId = undefined;
     document.querySelector('#sphereImage').style.filter = 'none';
-    allowInteraction(true); //TODO: should wait for the web socket to reconnect
+    //allowInteraction(true); //TODO: should wait for the web socket to reconnect
     
     //return to last active page:
     showSlideIndex(lastSplideIndex);
